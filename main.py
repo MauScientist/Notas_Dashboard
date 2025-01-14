447c3191b9ffb1d4a86d52272ae1f3e232522d70
MAURICIO A. RIBEIRO <mauricio.ribeiro.tech@gmail.com>
2025-01-13 21:27:57 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,33 +9,21 @@ st.set_page_config(
 
 #### Lendo banco de dados CSV
 ## sexto ano
-df = pd.read_csv('NotasSextoAno.csv')
-df['Scores 1'] = (df['Nota 1'] - df['Nota 1'].mean()) / df['Nota 1'].std()
-df['Scores 2'] = (df['Nota 2'] - df['Nota 2'].mean()) / df['Nota 2'].std()
-df['Scores 3'] = (df['Nota 3'] - df['Nota 3'].mean()) / df['Nota 3'].std()
-df_table = df[['Nome','Scores 1', 'Scores 2', 'Scores 3']]
-df['Média Anual'] = df[['Nota 1', 'Nota 2', 'Nota 3']].mean(axis=1)
-## setimo ano
-df1 = pd.read_csv('NotasSetimoAno.csv')
-df1['Scores 1'] = (df1['Nota 1'] - df1['Nota 1'].mean()) / df1['Nota 1'].std()
-df1['Scores 2'] = (df1['Nota 2'] - df1['Nota 2'].mean()) / df1['Nota 2'].std()
-df1['Scores 3'] = (df1['Nota 3'] - df1['Nota 3'].mean()) / df1['Nota 3'].std()
-df1_table = df1[['Nome','Scores 1', 'Scores 2', 'Scores 3']]
-df1['Média Anual'] = df1[['Nota 1', 'Nota 2', 'Nota 3']].mean(axis=1)
-## oitavo ano
-df2 = pd.read_csv('NotasOitavoAno.csv')
-df2['Scores 1'] = (df2['Nota 1'] - df2['Nota 1'].mean()) / df2['Nota 1'].std()
-df2['Scores 2'] = (df2['Nota 2'] - df2['Nota 2'].mean()) / df2['Nota 2'].std()
-df2['Scores 3'] = (df2['Nota 3'] - df2['Nota 3'].mean()) / df2['Nota 3'].std()
-df2_table = df2[['Nome','Scores 1', 'Scores 2', 'Scores 3']]
-df2['Média Anual'] = df2[['Nota 1', 'Nota 2', 'Nota 3']].mean(axis=1)
-## Nono ano
-df3 = pd.read_csv('NotasNonoAno.csv')
-df3['Scores 1'] = (df3['Nota 1'] - df3['Nota 1'].mean()) / df3['Nota 1'].std()
-df3['Scores 2'] = (df3['Nota 2'] - df3['Nota 2'].mean()) / df3['Nota 2'].std()
-df3['Scores 3'] = (df3['Nota 3'] - df3['Nota 3'].mean()) / df3['Nota 3'].std()
-df3_table = df3[['Nome','Scores 1', 'Scores 2', 'Scores 3']]
-df3['Média Anual'] = df3[['Nota 1', 'Nota 2', 'Nota 3']].mean(axis=1)
+
+
+def DataFrame_notas(df):
+    df['Media Plataforma'] = df[['Plataforma 1','Plataforma 2', 'Plataforma 3']].mean(axis=1)
+    df['NotaERec1'] = df[['Nota 1','Recuperação 1']].max(axis=1)
+    df['NotaERec2'] = df[['Nota 2','Recuperação 2']].max(axis=1)
+    df['NotaERec3'] = df[['Nota 3','Recuperação 3']].max(axis=1)
+    df['Nota Final'] = round(df[['NotaERec1','NotaERec2', 'NotaERec3']].mean(axis=1),2)
+    df['Scores'] = (df['Nota Final'] - 6.0) / df['Nota Final'].std()
+    df['Score Medio'] = df['Scores'].mean()
+    df['Situação'] = df['Nota Final'] >= 6.0
+    df['Situação'] = df['Situação'].map({True: 'Aprovado', False: 'Reprovado'})
+    df_scores = df[['Nome','Scores']]
+    return df, df_scores
+
 
 #df = pd.DataFrame(
 #    np.random.rand(30,9),
@@ -44,236 +32,61 @@ df3['Média Anual'] = df3[['Nota 1', 'Nota 2', 'Nota 3']].mean(axis=1)
 
 
 st.sidebar.title("Sistema de Análise de notas")
-
 st.sidebar.header("Ano Letivo de 2024")
 st.sidebar.text(" Disciplina de Matemática")
 ###### SEXTO ANO
-if st.sidebar.button("Sexto Ano"):
-    with st.expander("Notas Primeiro trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df,x = 'Nome', y=['Nota 1', 'Plataforma 1'],stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df, x='Nome', y=['Scores 1'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Segundo trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df, x='Nome', y=['Nota 2', 'Plataforma 2'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df, x='Nome', y=['Scores 2'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Terceiro trimestre", False):
-        st.bar_chart(df,x='Nome', y=['Nota 1', 'Plataforma 1'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df['Nota 1'])))
-    col1, col2, col3, col4 = st.columns(4, vertical_alignment="bottom", border=True)
-    with col1:
-        st.metric(label="Média turma", value=str(round(df['Nota 1'].mean(), 2)))
-    with col2:
-        st.metric(label="Nota Paraná", value=str(round(df['Nota 1'].mean(), 2)))
-    with col3:
-        st.metric(label="Plataforma", value=str(round(df['Nota 1'].mean(), 2)))
-    with col4:
-        st.metric(label="Score 1", value=str(round(df['Scores 1'].mean(), 2)))
-    st.text('')
-    st.bar_chart(df, x='Nome', y=['Média Anual'], stack=False)
-    st.bar_chart(df, x='Nome', y=['Plataforma 1', 'Plataforma 2', 'Plataforma 3'],
-                 stack=False)
+def Analise_alunos(TURMA_LETIVA, PLATAFORMA,df,df_scores):
+    if st.sidebar.button(TURMA_LETIVA):
+        st.header("Parâmetros do "+TURMA_LETIVA)
+        with st.expander("Notas Primeiro trimestre", False):
+            st.markdown('### Notas e suas recuperações')
+            st.bar_chart(df,x = 'Nome', y=['Nota 1','Recuperação 1', 'Nota 2', 'Recuperação 2', 'Nota 3', 'Recuperação 3'],stack=False)
+            st.markdown('## Nota Final ')
+            st.bar_chart(df,x = 'Nome', y=['Nota Final'],stack=False)
+            col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
+            with col1:
+                st.metric(label="Média turma", value=str(round(df['Nota Final'].mean(), 3)))
+            with col2:
+                st.metric(label="Média Plataformas", value=str(round(df['Media Plataforma'].mean(), 3)))
+            with col3:
+                st.metric(label="Numero de alunos ativos", value=str(len(df['Nome'])))
+            st.markdown("### Gráfico de Scores por aluno")
+            st.bar_chart(df, x='Nome', y=['Scores'], stack = False)
+            st.text('**Notas 1 = 3.0, Nota 2 = 2.0 e Nota 3 = 5.0 (Provão)')
+            st.markdown('## Tabela dos 3 maiores Scores Z do Sexto Ano')
+            st.table(df_scores.nlargest(3, 'Scores').round(2))
+        with st.expander("Situação do aluno", False):
+            st.markdown("## Situação do Aluno")
+            st.table(df[['Nome','Nota 1','Recuperação 1','Nota 2','Recuperação 2','Nota 3','Recuperação 3', 'Nota Final', 'Situação']])
+        with st.expander("Métricas Importantes", False):
+            st.markdown("## Métricas Importantes")
+            col1, col2, col3  = st.columns(3, vertical_alignment="bottom", border=True)
+            with col1:
+                st.metric(label="Média turma", value=str(round(df['Nota Final'].mean(),2)))
+            with col2:
+                st.metric(label="Porcentagem Nota Paraná", value=str(round(df['Nota 1'].mean(), 2)))
+            with col3:
+                st.metric(label="Índice Médio "+PLATAFORMA, value=str(round(df['Media Plataforma'], 2)))
+        with st.expander("Análise Estatistica por Série"):
+             st.markdown("## Análise Estatistica por Série")
+             st.table(df.describe().apply(lambda x: round(x,1)))
+        with st.expander("Acerta Brasil"):
+            st.markdown("Em breve Notas do Acerta Brasil")
 
-###### SEXTO ANO
-if st.sidebar.button("Sétimo Ano"):
-    with st.expander("Notas Primeiro trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df1,x = 'Nome', y=['Nota 1', 'Plataforma 1'],stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df1['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df1['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df1['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df1, x='Nome', y=['Scores 1'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df1_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Segundo trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df1, x='Nome', y=['Nota 2', 'Plataforma 2'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df1['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df1['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df1['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df1, x='Nome', y=['Scores 2'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df1_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Terceiro trimestre", False):
-        st.bar_chart(df1,x='Nome', y=['Nota 1', 'Plataforma 1'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df1['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df1['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df1['Nota 1'])))
-    col1, col2, col3, col4 = st.columns(4, vertical_alignment="bottom", border=True)
-    with col1:
-        st.metric(label="Média turma", value=str(round(df1['Nota 1'].mean(), 2)))
-    with col2:
-        st.metric(label="Nota Paraná", value=str(round(df1['Nota 1'].mean(), 2)))
-    with col3:
-        st.metric(label="Plataforma", value=str(round(df1['Nota 1'].mean(), 2)))
-    with col4:
-        st.metric(label="Score 1", value=str(round(df1['Scores 1'].mean(), 2)))
-    st.text('')
-    st.bar_chart(df1, x='Nome', y=['Média Anual'], stack=False)
-    st.bar_chart(df1, x='Nome', y=['Plataforma 1', 'Plataforma 2', 'Plataforma 3'],
-                 stack=False)
+df6 = pd.read_csv('NotasSextoAno_2T.csv', sep=",")
+df6A,df6AScores = DataFrame_notas(df6)
+Analise_alunos("6A","Matific",df6A,df6AScores)
 
-### OITAVO ANO
+df7 = pd.read_csv('NotasSetimoAno_2T.csv', sep=",")
+df7A,df7AScores = DataFrame_notas(df7)
+Analise_alunos("7A","Matific",df7A,df7AScores)
 
-if st.sidebar.button("Oitavo Ano"):
-    with st.expander("Notas Primeiro trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df2,x = 'Nome', y=['Nota 1', 'Plataforma 1'],stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df2['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df2['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df2['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df2, x='Nome', y=['Scores 1'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df2_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Segundo trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df2, x='Nome', y=['Nota 2', 'Plataforma 2'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df2['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df2['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df2['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df2, x='Nome', y=['Scores 2'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df2_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Terceiro trimestre", False):
-        st.bar_chart(df2,x='Nome', y=['Nota 1', 'Plataforma 1'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df2['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df2['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df2['Nota 1'])))
-    col1, col2, col3, col4 = st.columns(4, vertical_alignment="bottom", border=True)
-    with col1:
-        st.metric(label="Média turma", value=str(round(df2['Nota 1'].mean(), 2)))
-    with col2:
-        st.metric(label="Nota Paraná", value=str(round(df2['Nota 1'].mean(), 2)))
-    with col3:
-        st.metric(label="Plataforma", value=str(round(df2['Nota 1'].mean(), 2)))
-    with col4:
-        st.metric(label="Score 1", value=str(round(df2['Scores 1'].mean(), 2)))
-    st.text('')
-    st.bar_chart(df2, x='Nome', y=['Média Anual'], stack=False)
-    st.bar_chart(df2, x='Nome', y=['Plataforma 1', 'Plataforma 2', 'Plataforma 3'],
-                 stack=False)
-### Nono Ano
+df8 = pd.read_csv('NotasOitavoAno_2T.csv', sep=",")
+df8A,df7AScores = DataFrame_notas(df8)
+Analise_alunos("8A","Khan Academy", df8A,df7AScores)
 
-if st.sidebar.button("Nono Ano"):
-    with st.expander("Notas Primeiro trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df3,x = 'Nome', y=['Nota 1', 'Plataforma 1'],stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df3['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df3['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df3['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df3, x='Nome', y=['Scores 1'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df3_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Segundo trimestre", False):
-        st.text('Comparativo de notas e plataformas')
-        st.bar_chart(df3, x='Nome', y=['Nota 2', 'Plataforma 2'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df3['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df3['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df3['Nome'])))
-        st.markdown("Gráfico de Scores por aluno")
-        st.bar_chart(df3, x='Nome', y=['Scores 2'])
-        st.text('Tabela dos 3 maiores Scores Z do Sexto Ano')
-        st.table(df3_table.nlargest(3, 'Scores 1').round(2))
-    with st.expander("Notas Terceiro trimestre", False):
-        st.bar_chart(df3,x='Nome', y=['Nota 1', 'Plataforma 1'], stack=False)
-        col1, col2, col3 = st.columns(3, vertical_alignment="bottom", border=True)
-        with col1:
-            st.metric(label="Média turma", value=str(round(df3['Nota 1'].mean(), 3)))
-        with col2:
-            st.metric(label="Média Plataformas", value=str(round(df3['Plataforma 1'].mean(), 3)))
-        with col3:
-            st.metric(label="Numero de alunos ativos", value=str(len(df3['Nota 1'])))
-    col1, col2, col3, col4 = st.columns(4, vertical_alignment="bottom", border=True)
-    with col1:
-        st.metric(label="Média turma", value=str(round(df3['Nota 1'].mean(), 2)))
-    with col2:
-        st.metric(label="Nota Paraná", value=str(round(df3['Nota 1'].mean(), 2)))
-    with col3:
-        st.metric(label="Índice Plataforma", value=str(round(df3['Nota 1'].mean(), 2)))
-    with col4:
-        st.metric(label="Score", value=str(round(df3['Scores 1'].mean(), 2)))
-    st.text('')
-    st.bar_chart(df3, x='Nome', y=['Média Anual'], stack=False)
-    st.bar_chart(df3, x='Nome', y=['Plataforma 1', 'Plataforma 2', 'Plataforma 3'],
-                 stack=False)
+df9 = pd.read_csv('NotasOitavoAno_2T.csv', sep=",")
+df9A,df9AScores = DataFrame_notas(df9)
+Analise_alunos("9A","Khan Academy",df9A,df9AScores)
 
-
-if st.sidebar.button("Análise Estatistica por Série"):
-    st.markdown("Análise Estatistica por Série")
-    with st.expander("Estatistica Sexto Ano", expanded=False):
-       st.table(df.describe().apply(lambda x: round(x,1)))
-
-    with st.expander("Estatistica Setimo Ano", expanded=False):
-       st.table(df1.describe().apply(lambda x: round(x,1)))
-
-    with st.expander("Estatistica Oitavo Ano", expanded=False):
-       st.table(df2.describe().apply(lambda x: round(x,1)))
-
-    with st.expander("Estatistica Nono Ano", expanded=False):
-       st.table(df2.describe().apply(lambda x: round(x,1)))
-
-if st.sidebar.button("Estatística do Acerta Brasil"):
-    st.markdown("## Acerta Brasil")
+st.sidebar.info(': mau.ap.ribeiro@gmail.com', icon=":material/mail:")
